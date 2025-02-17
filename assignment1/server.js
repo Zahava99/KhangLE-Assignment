62d8817fe4aef3a9ce793f64f4f395af8a050491
Zahava99 <97643126+Zahava99@users.noreply.github.com>
2025-02-17 11:23:18 +0700

    Test1

--- assignment1/server.js
+++ assignment1/server.js
@@ -9,7 +9,9 @@ connectDB();
 
 app.use(cors());
 app.use(bodyParser.json());
+// app.use(express.json());
 
+app.use('/api/users', require('./routes/user.routes'));
 app.use('/api/categories', require('./routes/category.routes'));
 app.use('/api/products', require('./routes/product.routes'));
 
